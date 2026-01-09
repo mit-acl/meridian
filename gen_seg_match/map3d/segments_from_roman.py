675a3c6df7dc025c6162c2e67076edc108055486
Mason Peterson <mbpeterson70@gmail.com>
2026-01-09 10:21:24 -0500

    initial lines from plane borders extraction
    * currently convex hull is retrieved and all borders that are long
      enough are turned into a line. No points for now.

--- gen_seg_match/map3d/segments_from_roman.py
+++ gen_seg_match/map3d/segments_from_roman.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List, Tuple
+import shapely
 
 from roman.object.segment import Segment as RomanSegment
 from gen_seg_match.segment.segment_types import (
@@ -292,9 +293,141 @@ class GeneralSegmentConverter:
                 < eigvals[2] / eigvals[1]
                 < self.params.max_eigval_ratio
             ):
-                general_segments.append(self.to_plane_segment(roman_segment, mean, U))
+                general_segments.extend(
+                    self.get_plane_with_occlusion(
+                        roman_segment,
+                        roman_segment.occluded_points,
+                        mean,
+                        U,
+                    )
+                )
             else:
                 general_segments.append(self.to_point_segment(roman_segment, mean))
 
         general_segments.reindex()
         return general_segments
+    
+    def get_plane_with_occlusion(
+        self, segment: RomanSegment, occluded_points: np.ndarray, mean: np.ndarray, U: np.ndarray
+    ) -> SegmentList:
+        normal = U[:, 2]
+        dense_points = segment.points if self.params.copy_dense_points else None
+
+        sparse_segments = SegmentList()
+
+        sparse_segments.append(SegmentPlane(
+            id=segment.id,
+            point=mean,  # avoid using roman_segment.center in case _center_ref == 'bottom-middle'
+            normal=normal,
+            ratio_feature=self.get_roman_ratio_feature(segment),
+            cos_feature=segment.semantic_descriptor,
+            first_seen=segment.first_seen,
+            last_seen=segment.last_seen,
+            dense_points=dense_points,
+            history=getattr(segment, "history", []),
+        ))
+        
+        # project points and occluded points to 2D plane
+        plane_basis_vectors = U[:, :2]  # first two principal components
+        points_2d = self.get_points_on_2d_plane(segment.points, normal, mean, plane_basis_vectors)
+        occluded_points_2d = self.get_points_on_2d_plane(occluded_points, normal, mean, plane_basis_vectors)
+        line_borders = self.get_line_borders_from_2d_points(points_2d, occluded_points_2d)
+        for pt1, pt2 in line_borders:
+            sparse_segments.append(SegmentLine.from_endpoints(
+                id=segment.id,
+                pt1=mean + pt1[0] * plane_basis_vectors[:, 0] + pt1[1] * plane_basis_vectors[:, 1],
+                pt2=mean + pt2[0] * plane_basis_vectors[:, 0] + pt2[1] * plane_basis_vectors[:, 1],
+                ratio_feature=self.get_roman_ratio_feature(segment),
+                cos_feature=segment.semantic_descriptor,
+                first_seen=segment.first_seen,
+                last_seen=segment.last_seen,
+                dense_points=None,
+                history=getattr(segment, "history", []),
+            ))
+            
+        return sparse_segments
+        
+    def get_points_on_2d_plane(self, points: np.ndarray, plane_normal: np.ndarray, plane_offset: np.ndarray, plane_basis_vectors: np.ndarray) -> np.ndarray:
+        """
+        Projects 3D points onto a 2D plane defined by its normal, offset, and basis vectors.
+
+        Args:
+            points (np.ndarray): Nx3 array of 3D points to be projected.
+            plane_normal (np.ndarray): 3 dimensional normal vector of the plane.
+            plane_offset (np.ndarray): 3 dimensional point on the plane.
+            plane_basis_vectors (np.ndarray): 3x2 array where each column is a basis vector of the plane.
+
+        Returns:
+            np.ndarray: Nx2 array of 2D projected points.
+        """
+        
+        # Project points onto plane
+        plane_normal = plane_normal / np.linalg.norm(plane_normal)
+        e1 = plane_basis_vectors[:, 0] / np.linalg.norm(plane_basis_vectors[:, 0])
+        e2 = plane_basis_vectors[:, 1] / np.linalg.norm(plane_basis_vectors[:, 1])
+        R = np.column_stack((e1, e2, plane_normal))
+        points_on_plane = (np.eye(3) - np.outer(plane_normal, plane_normal)) @ (points - plane_offset).T
+        points_on_2d = R.T @ points_on_plane
+        return points_on_2d[:2, :].T
+    
+    def get_line_borders_from_2d_points(self, points_2d: np.ndarray, occluded_points_2d: np.ndarray) -> List[Tuple[np.ndarray, np.ndarray]]:
+        """
+        Computes the line borders of a 2D point cloud. If line borders are all close to occluded points,
+            or if lines are short, they are not included.
+
+        Args:
+            points_2d (np.ndarray): Nx2 array of 2D points.
+            occluded_points_2d (np.ndarray): Nx2 array of 2D occluded points.
+
+        Returns:
+            SegmentList: sparse line segments representing the line borders.
+        """
+        lines = []
+        if len(points_2d) == 0:
+            return lines
+        
+        convex_hull_shapely = shapely.convex_hull(shapely.MultiPoint(points_2d))
+        convex_hull = np.array(convex_hull_shapely.exterior.coords)
+        
+        # if edge angles are small, combine them
+        smoothed_convex_hull = [convex_hull[0]]
+        for i in range(1, len(convex_hull) - 1):
+            pt_prev = convex_hull[i - 1]
+            pt_curr = convex_hull[i]
+            pt_next = convex_hull[i + 1]
+            v1 = pt_curr - pt_prev
+            v2 = pt_next - pt_curr
+            v1 = v1 / np.linalg.norm(v1)
+            v2 = v2 / np.linalg.norm(v2)
+            angle = np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0))
+            # if angle < np.deg2rad(self.params.plane_border_smoothing_angle_deg):
+            if angle < np.deg2rad(20.0): # TODO: magic number
+                # skip current point
+                continue
+            else:
+                smoothed_convex_hull.append(pt_curr)
+        convex_hull = smoothed_convex_hull
+        
+        for i, pt0 in enumerate(np.array(convex_hull)):
+            pt1 = np.array(convex_hull)[i + 1 if i + 1 < len(convex_hull) else 0]
+            # first check if the line is long enough
+            if np.linalg.norm(pt1 - pt0) > self.params.non_point_min_extent:
+                # if not occluded poitns, add line directly
+                if occluded_points_2d.size == 0:
+                    lines.append((pt0, pt1))
+                    continue
+                
+                # check if the line is far enough from occluded points
+                occluded = False
+                # for frac in np.linspace(0, 1, num=10):
+                #     test_pt = pt0 + frac * (pt1 - pt0)
+                #     dists_to_occluded = np.linalg.norm(occluded_points_2d - test_pt, axis=1)
+                #     # TODO: magic number - num = 10 and 1.0
+                #     if np.min(dists_to_occluded) < 1.0:
+                #         occluded = True
+                #         break
+                if not occluded:
+                    lines.append((pt0, pt1))
+                    
+        return lines
+    
