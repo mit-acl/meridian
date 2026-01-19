4ebe3ac42945f8c93704290c26cafc744b4f3037
Mason Peterson <mbpeterson70@gmail.com>
2026-01-19 13:09:10 -0500

    move segment point clean up to utils

--- gen_seg_match/map2d/ground_segmenter.py
+++ gen_seg_match/map2d/ground_segmenter.py
@@ -9,6 +9,7 @@ from gen_seg_match.params import GroundSegmenterParams
 from gen_seg_match.segment.aerial_segment import AerialSegment
 from gen_seg_match.segment.segment_types import SegmentList, GeneralSegment
 from gen_seg_match.map3d.submap import Submap
+from gen_seg_match.utils import clean_up_points
 
 
 class GroundSegmenter:
@@ -45,40 +46,10 @@ class GroundSegmenter:
         return aerial_segments
 
     def cleanup_points(self, segment: GeneralSegment):
-        if segment.dense_points is not None:
-            pcd = o3d.geometry.PointCloud()
-            pcd.points = o3d.utility.Vector3dVector(segment.dense_points)
-            pcd_sampled = pcd.voxel_down_sample(voxel_size=segment.voxel_size)
-            if self.params.outlier_removal_std is not None:
-                pcd_pruned, _ = pcd_sampled.remove_statistical_outlier(
-                    10, self.params.outlier_removal_std
-                )
-            else:
-                pcd_pruned = pcd_sampled
-
-            if pcd_pruned.is_empty():
-                segment.dense_points = None
-            else:
-                segment.dense_points = np.asarray(pcd_pruned.points)
-
-        if segment.dense_points is not None:
-            # Perform DBSCAN clustering
-            labels = np.array(
-                segment.pcd.cluster_dbscan(
-                    eps=self.params.dbscan_epsilon,
-                    min_points=self.params.dbscan_min_points,
-                )
-            )
-
-            # Number of clusters, ignoring noise if present
-            max_label = labels.max()
-
-            # get largest cluster
-            cluster_sizes = np.zeros(max_label + 1)
-            for i in range(max_label + 1):
-                cluster_sizes[i] = np.sum(labels == i)
-            max_cluster = np.argmax(cluster_sizes)
-
-            # Filter out any points not belonging to max cluster
-            filtered_indices = np.where(labels == max_cluster)[0]
-            segment.dense_points = segment.dense_points[filtered_indices]
+        segment.dense_points = clean_up_points(
+            segment.dense_points,
+            voxel_size=segment.voxel_size,
+            outlier_removal_std=self.params.outlier_removal_std,
+            dbscan_epsilon=self.params.dbscan_epsilon,
+            dbscan_min_points=self.params.dbscan_min_points,
+        )
