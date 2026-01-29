0aa8dc4a2a9271bca251d87651311b9311d7ab5b
Mason Peterson <mbpeterson70@gmail.com>
2026-01-29 08:55:40 -0500

    add planes back in
    somehow in getting merged in, planes were not being used for matching

--- gen_seg_match/match/segment_matcher.py
+++ gen_seg_match/match/segment_matcher.py
@@ -38,9 +38,8 @@ class SegmentMatcher:
     ):
         map1 = SegmentList(deepcopy(map1))
         map2 = SegmentList(deepcopy(map2))
-        # TODO: incorporate planes
-        map1 = map1.get_points() + map1.get_lines()
-        map2 = map2.get_points() + map2.get_lines()
+        map1 = map1.get_points() + map1.get_lines() + map1.get_planes()
+        map2 = map2.get_points() + map2.get_lines() + map2.get_planes()
 
         # return empty associations if map is empty
         if len(map1) == 0 or len(map2) == 0:
