f06462499adfde9faffc8a28a48c0c7b4a14ea98
Mason Peterson <mbpeterson70@gmail.com>
2026-04-09 08:53:13 -0400

    change default segment_match knn from 5 to 10

--- gen_seg_match/params/segment_match_params.py
+++ gen_seg_match/params/segment_match_params.py
@@ -86,7 +86,7 @@ class SegmentMatchParams(ParamsBase):
     xy_dir_constrained_2d: bool = False
     rot_unc_ang_rad: float = np.deg2rad(20.0)
     point_noise_from_angle: bool = True
-    k_nearest_neighbors: int = 5  # None = all-to-all
+    k_nearest_neighbors: int = 10  # None = all-to-all
 
     solver: str = "langevin"  # "clipper" or "langevin"
     langevin_params: LangevinMatcherParams = None
